18668702e2d8ad2cfe69472d6fff047f21f6e0f5
joshpson <jpearson846@gmail.com>
2018-05-29 14:58:44 -0400

    Working lending model

--- app/models/lending.rb
+++ app/models/lending.rb
@@ -1,5 +1,5 @@
 class Lending < ApplicationRecord
 	belongs_to :copy
-  has_one :user, through: :copies
-	belongs_to :borrower, class_name: :user
+  has_one :user, through: :copy
+	belongs_to :borrower, :class_name => "User"
 end
