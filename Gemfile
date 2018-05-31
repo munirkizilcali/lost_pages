b7e06de6c4d2e22881a5e09eab164b8c0d889c29
munirkizilcali <munir.kizilcali@gmail.com>
2018-05-31 16:10:02 -0400

    facebook-login

--- Gemfile
+++ Gemfile
@@ -28,7 +28,9 @@ gem 'jbuilder', '~> 2.5'
 gem 'bcrypt', '~> 3.1.7'
 gem 'json', '~> 2.1'
 
-
+gem 'omniauth', '~> 1.8', '>= 1.8.1'
+gem 'omniauth-facebook', '~> 5.0'
+gem 'dotenv-rails', '~> 2.4'
 # Use ActiveStorage variant
 # gem 'mini_magick', '~> 4.8'
 
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,14 +2,28 @@ class SessionsController < ApplicationController
 
 
 	def create
-		@user = User.find_by(email: params[:email])
-		if @user
-			return head(:forbidden) unless @user.authenticate(params[:password])
-			session["user_id"] = @user.id
-			redirect_to root_path
-		else
-			flash[:message] = "Username or password did not match"
-			render :new
+
+		if auth #if login with omniauth-facebook
+		    @user = User.find_or_create_by(uid: auth['uid']) do |u|
+		      u.name = auth['info']['name']
+		      u.email = auth['info']['email']
+		      u.image = auth['info']['image']
+		      u.password_digest = "0"
+		    end
+		    session[:user_id] = @user.id
+		    redirect_to root_path
+
+		else # if login with password
+
+			@user = User.find_by(email: params[:email])
+			if @user
+				return head(:forbidden) unless @user.authenticate(params[:password])
+				session["user_id"] = @user.id
+				redirect_to root_path
+			else
+				flash[:message] = "Username or password did not match"
+				render :new
+			end
 		end
 	end
 
@@ -17,5 +31,11 @@ class SessionsController < ApplicationController
 		session.delete :user_id
 		redirect_to root_path
 	end
+
+	private 
+
+	def auth
+    	request.env['omniauth.auth']
+  	end
 
 end
