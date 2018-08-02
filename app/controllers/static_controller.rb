d99f4b615fc0a849a62547d09bceb983fc507b03
munirkizilcali <munir.kizilcali@gmail.com>
2018-08-02 10:34:50 -0400

    privacy notice added for deployment

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -4,4 +4,8 @@ class StaticController < ApplicationController
 
   end
 
+  def privacy
+
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -22,5 +22,7 @@ Rails.application.routes.draw do
 
   get '/friends' => 'friendships#index'
 
+  get  'static/privacy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
